920d212a58a3e6b9018d0bd17fe5937c53142f1a
Seoyoung Lee <selee@student.42lyon.fr>
2023-01-10 15:17:45 +0100

    localhost working but not login.42.fr

--- srcs/requirements/wordpress/script/run.sh
+++ srcs/requirements/wordpress/script/run.sh
@@ -1,7 +1,6 @@
 if ! wp --allow-root --path='/var/www/html/wordpress/' core is-installed; then
-		wp --allow-root --path='/var/www/html/wordpress/' core install -url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN
-		--admin_password=$WP_PASS --admin_email=$WP_AMAIL --skip-email --quiet
-		wp --allow-root --path='/var/www/html/wordpress/' user create $WP_USER $WP_MAIL --user_pass=$WP_PASS --porcelain
-		wp --allow-root option set siteurl $WP_URL --quiet
+	wp --allow-root --path='/var/www/html/wordpress/' core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_APASS --admin_email=$WP_AMAIL --skip-email --quiet
+	wp --allow-root --path='/var/www/html/wordpress/' user create $WP_USER $WP_MAIL --user_pass=$WP_PASS --porcelain
+	# wp --allow-root option set siteurl $WP_URL --quiet
 fi
 php-fpm7.3 -R -F
